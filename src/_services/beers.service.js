2b12e685673e25e95ad73b9e834f15323739e9a6
Andrzej Bogacki <andrzej.bogacki@intive.com>
2019-07-19 15:07:24 +0200

    Create beersService to pipe event stream from input to fetch beers and make HelloWorld component subscribe to it

--- src/_services/beers.service.js
+++ src/_services/beers.service.js
@@ -1,20 +1,34 @@
 import { Subject } from 'rxjs'
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators'
 import axios from 'axios'
 
 const subject = new Subject()
 
 const baseUrl = 'https://api.punkapi.com/v2/'
+const injectPageNumberIntoQueryUrl = (pageNumber = 1) =>
+  `beers?page=${pageNumber}&per_page=10`
+
+const fetchBeers = pageNumbers => {
+  return pageNumbers.pipe(
+    debounceTime(400),
+    distinctUntilChanged(),
+    switchMap(({ event }) => fetchBeerEntries(event.target.value))
+  )
+}
+
+const fetchBeerEntries = async pageNumber => {
+  try {
+    const response = await axios.get(
+      baseUrl + injectPageNumberIntoQueryUrl(pageNumber || 1)
+    )
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
 
 export const beersService = {
-  fetchBeers: async params => {
-    try {
-      const result = await axios.get(baseUrl, {
-        params,
-      })
-      subject.next(result.data)
-    } catch (error) {
-      return 'Error has occured'
-    }
-  },
+  fetchBeers,
+  fetchBeerEntries,
   getBeers: () => subject.asObservable(),
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,38 @@
 <template>
   <div class="hello">
     <p>
-      <button type="text" v-stream:click="count$">+</button>
+      <input type="text" v-stream:input="pageNumber$" />
     </p>
-    <p>{{count}}</p>
+    <div class="beer" v-for="beer in beers" :key="beer.id">
+      <img :src="beer.image_url" />
+      <p>{{ beer.name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import { beersService } from '@/_services/beers.service.js'
 import { map, startWith, scan, debounceTime } from 'rxjs/operators'
-import { Observable, Subject } from 'rxjs'
+import { Subject, Observable } from 'rxjs'
+import debounce from 'lodash/debounce'
 
 export default {
   name: 'HelloWorld',
-  subscriptions() {
-    this.count$ = new Subject()
+  data() {
     return {
-      count: this.count$.pipe(
-        debounceTime(500),
-        map(() => 1),
-        startWith(0),
-        scan((total, change) => total + change)
-      ),
+      beers: [],
     }
+  },
+  subscriptions() {
+    this.pageNumber$ = new Subject()
+    return {
+      pageNumber: this.pageNumber$,
+    }
+  },
+  created() {
+    beersService
+      .fetchBeers(this.pageNumber$)
+      .subscribe(results => (this.beers = results))
   },
 }
 </script>
@@ -49,5 +58,18 @@ input {
   padding: 6px;
   border-radius: 12px;
   border: 1px solid gray;
+}
+
+.beer {
+  padding: 20px;
+  display: inline-block;
+  border: 1px solid gray;
+  border-radius: 12px;
+  margin: 20px;
+}
+
+img {
+  max-width: 200px;
+  max-height: 200px;
 }
 </style>
