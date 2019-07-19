7bac24cb6bae7b4147c373c1b90e85786a32b595
andrzej.bogacki <andrzej.bogacki@ws-c02yq0jqlvdl.corp.smtsoftware.com>
2019-07-19 10:27:07 +0200

    Creates message service

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    {{messages}}
+    <MessagesButtons />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+import MessagesButtons from '@/components/MessagesButtons'
+import { messageService } from '@/_services/message.service'
 
 export default {
   name: 'app',
+  data() {
+    return { messages: [] }
+  },
+  created() {
+    this.subscription = messageService.getMessages().subscribe(message => {
+      if (message) {
+        this.messages.push(message)
+      } else {
+        this.messages = []
+      }
+    })
+  },
+  beforeDestroy() {
+    this.subscription.unsubscribe()
+  },
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+    MessagesButtons,
+  },
 }
 </script>
 
---
+++ src/components/MessagesButtons.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="sendMessage()">send message</button>
+    <button @click="clearMessages()">clear messages</button>
+  </div>
+</template>
+
+<script>
+import { messageService } from '@/_services'
+export default {
+  data() {
+    return {
+      messages: [],
+    }
+  },
+  methods: {
+    sendMessage() {
+      messageService.sendMessage(
+        'Message from Home Page Component to App Component!'
+      )
+    },
+    clearMessages() {
+      messageService.clearMessages()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
