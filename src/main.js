f1ae25f7345faeaed441fce35db6cc860d0a51bb
abogacki <pacojoyek@gmail.com>
2019-07-21 12:51:51 +0200

    Adds buefy package

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRx from 'vue-rx'
+import buefy from 'buefy'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRx)
-
+Vue.use(buefy)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
